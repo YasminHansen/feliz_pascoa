418b954a7acdef90930284d047f708c8ef90a24c
YasminHansen <yaa-h@hotmail.com>
2019-04-23 17:25:20 -0300

    Arquivo de modulo de Pascoa do Drupal 8 v.10

---
+++ src/Controller/FelizPascoaController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\feliz_pascoa\Controller;
+
+class FelizPascoaController {
+	public function felizpascoa(){
+		return array(
+			'#title' => 'Feliz Páscoa!',
+			'#markup' => 'Feliz páscoa!'
+
+		);
+	}
+
+}
